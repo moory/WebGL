2ac3ed83c91560f1b281c6582f3a1e4470b70ff2
moory <majili@druid.tech>
2019-05-22 22:23:55 +0800

    update

--- sc/main.js
+++ sc/main.js
@@ -4,19 +4,19 @@ Game.rooms['W14N11'].createConstructionSite(4,12,STRUCTURE_ROAD)
 //--------------------------------
 const HOME=Game.spawns['Spawn1'];
 const ROOM=Game.rooms['W14N11'];
-const CREEP_TOTAL=8;
+const CREEP_TOTAL=10;
 const CREEP_NAME='Worker';
 
+
 module.exports.loop = function () {
+
     born();
 
     for(let k in Game.creeps){
-        if(k==='Worker5'||k==='Worker0'){
+        if(k==='Worker4'||k==='Worker5'||k==='Worker6'||k==='Worker7'||k==='Worker8'||k==='Worker9'||k==='Worker10'){
             upgradeRCL(Game.creeps[k])
-
         }else{
             work(Game.creeps[k],HOME,0)
-            // work(Game.creeps[k],ROOM.controller,1)
         }
 
     }
@@ -27,18 +27,15 @@ const born=()=>{
     for(let i=0;i<CREEP_TOTAL;i++){
         if(!findCreeps(CREEP_NAME+i)){
             let err_code;
-            // if(i===0){
-            //      err_code=HOME.spawnCreep([BUILD, CARRY, MOVE], CREEP_NAME+i);
-            // }else{
             err_code=HOME.spawnCreep([WORK, CARRY, MOVE], CREEP_NAME+i);
-            // }
+            return;
         }
     }
 }
 
 const work=(c,t,s)=>{
     if(c.carry.energy < c.carryCapacity) {
-        var sources = c.room.find(FIND_SOURCES);
+        let sources = c.room.find(FIND_SOURCES);
         if(c.harvest(sources[s]) === ERR_NOT_IN_RANGE) {
             c.moveTo(sources[s]);
         }
@@ -64,9 +61,30 @@ const build=(c)=>{
 }
 
 const upgradeRCL=(c)=>{
-    if( c.upgradeController(ROOM.controller) === ERR_NOT_IN_RANGE ) {
-        c.moveTo(ROOM.controller);
+    if(c.carry.energy===0){
+        let sources = c.room.find(FIND_SOURCES);
+        if(c.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
+            c.moveTo(sources[1]);
+        }
+    }else if(c.carry.energy === c.carryCapacity){
+        if( c.upgradeController(ROOM.controller) === ERR_NOT_IN_RANGE ) {
+            c.moveTo(ROOM.controller);
+        }
+    }else if(calcPath(42,8,c)>calcPath(44,19,c)){//离矿更近
+        let sources = c.room.find(FIND_SOURCES);
+        if(c.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
+            c.moveTo(sources[1]);
+        }
+    }else{
+        if( c.upgradeController(ROOM.controller) === ERR_NOT_IN_RANGE ) {
+            c.moveTo(ROOM.controller);
+        }
     }
+
+}
+
+const calcPath=(x,y,c)=>{
+    PathFinder.search(Game.rooms['W14N11'].getPositionAt(x,y),c).cost
 }
 
 const findCreeps=(name)=>{
